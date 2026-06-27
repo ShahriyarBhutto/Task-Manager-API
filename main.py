aa329ea6db79bbe5e1d55f1f40902d7aafc01781
Shahriyar Bhutto <88304298+ShahriyarBhutto@users.noreply.github.com>
2026-06-27 17:02:19 +0500

    complete crud applied

--- main.py
+++ main.py
@@ -4,42 +4,79 @@ from pydantic import BaseModel
 app = FastAPI()
 
 
-temp_db = []
-task_id = len(temp_db)
+# temporary database, which will be replaced with database like SQLite
+temp_db = [
 
-class Task(BaseModel):
-    id:int = task_id + 1
-    title:str
-    description: str
-    done:bool = False
+{
+  "id": 1,
+  "title": "t1",
+  "description": "t1di",
+  "done": True
+},
+{
+  "id": 2,
+  "title": "t1",
+  "description": "t1di",
+  "done": True
+},
+{
+  "id": 3,
+  "title": "t1",
+  "description": "t1di",
+  "done": True
+}
+]
 
-class TaskModel(BaseModel):
+
+# CreateTask class for the structure of the task's data
+
+class CreateTask(BaseModel):
     title:str
     description:str
     done:bool = False
 
 
+# UpdateTask class for the structure of the tasks to be updated
+
+class UpdateTask(BaseModel):
+    title:str
+    description:str
+    done:bool
+
+
+
+# Create Task
+
+@app.post("/task")
+async def add_task(task:CreateTask):
+    mytask = {"id":len(temp_db)+1,"title":task.title,"description":task.description,"done":task.done}
+    temp_db.append(mytask)
+    return mytask    
+
+# Read Task
 
 @app.get("/")
 async def get_all_tasks():
     return temp_db
 
 
-@app.post("/task")
-async def add_task(task:TaskModel):
-    task = {"title":task.title,"description":task.description,"done":task.done}
-    temp_db.append(task)
-    return task
+
+# Update Task
 
 @app.put("/task/{id}")
-async def update_task(task:Task):
-    temp_task = {}
-    for task in temp_db:
-        if task.id == id:
-            pass
-    
+async def update_task(id:int,valueUpdate:UpdateTask):
+    for index,task in enumerate(temp_db):
+        if task["id"] == id:
+            temp_db[index] = {"id":id,"title": valueUpdate.title,"description":valueUpdate.description,"done":valueUpdate.done}
+            return{"message":"Task updated"}
+    return{"message":"Task not found"}
 
+# Delete task
 
 @app.delete("/delete/{id}")
-async def delete_task():
-    pass
+async def delete_task(id:int):
+    for index,task in enumerate(temp_db):
+        if task["id"] == id:
+            temp_db.pop(index)
+            return {"message":"Task Deleted"}
+    return {"message":"Task not found"}
