7957e8bd2c9454b3e4ff04209e668f8039414224
Shahriyar Bhutto <88304298+ShahriyarBhutto@users.noreply.github.com>
2026-06-29 10:58:07 +0500

    get db functionality

--- database.py
+++ database.py
@@ -10,3 +10,11 @@ Base = declarative_base()
 
 
 SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
